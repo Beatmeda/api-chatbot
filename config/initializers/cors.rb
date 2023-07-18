c847ece0dde5f5d875a65af59a6bcf2a6e84e8ca
Beatmeda <beatmeda@gmail.com>
2023-07-17 23:54:35 -0500

    Config env for cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins ENV.fetch("CORS_ORIGIN") { 'http://localhost:3001' }
 
     resource '/api/*',
              headers: :any,
